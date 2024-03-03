8a35c873ce56f294021411f206c604a0f1eb0c84
ofrimayer <ofrimayer@gmail.com>
2024-03-03 13:48:42 -0800

    constants.js + .gitignore modification

--- studysips/src/backend/db/index.js
+++ studysips/src/backend/db/index.js
@@ -1,10 +1,12 @@
+import { password, username } from constants.js
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',
+  user: username,
   host: 'studysips-db.cbkocewoeib2.us-east-2.rds.amazonaws.com',
   database: 'studysips-db',
-  password: 'studysips',
+  password: password,
   port: 5432,
   // For SSL connection in production, we might need additional configuration
 });
